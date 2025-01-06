72a1ff76f43cc18a1b4e456a499fe98778f97fff
sepehrsoh <59518359+sepehrsoh@users.noreply.github.com>
2025-01-06 17:10:19 +0330

    refactor lookup and lookup compound (#1)
    Co-authored-by: sepehrsoh <sepehr.sohrabpour@snapp.cab>

--- internal/lookup_compound.go
+++ internal/lookup_compound.go
@@ -35,57 +35,47 @@ func parseWords(phrase string, preserveCase bool, splitBySpace bool) []string {
 
 func (s *SymSpell) LookupCompound(phrase string, maxEditDistance int) *SuggestItem {
 	terms1 := parseWords(phrase, false, false)
-	var suggestions []SuggestItem
-	var suggestionParts []SuggestItem
-	isLastCombi := false
+	cp := compoundProcessor{
+		suggestions:     make([]SuggestItem, 0),
+		suggestionParts: make([]SuggestItem, 0),
+		replacedWords:   make(map[string]SuggestItem),
+		isLastCombi:     false,
+	}
 	for i, _ := range terms1 {
-		suggestions, _ = s.Lookup(terms1[i], verbositypkg.Top, maxEditDistance)
+		cp.terms1 = terms1[i]
+		cp.suggestions, _ = s.Lookup(cp.terms1, verbositypkg.Top, maxEditDistance)
 		// Combine adjacent terms
-		if i > 0 && !isLastCombi {
-			suggestionsCombi, _ := s.Lookup(terms1[i-1]+terms1[i], verbositypkg.Top, maxEditDistance)
+		if i > 0 && !cp.isLastCombi {
+			cp.terms2 = terms1[i-1]
+			suggestionsCombi, _ := s.Lookup(cp.terms2+cp.terms1, verbositypkg.Top, maxEditDistance)
 			if len(suggestionsCombi) > 0 {
-				best1 := suggestionParts[len(suggestionParts)-1]
-				var best2 SuggestItem
-				if len(suggestions) > 0 {
-					best2 = suggestions[0]
-				} else {
-					best2 = createWithProbability(terms1[i], maxEditDistance+1)
-				}
-				distance1 := best1.Distance + best2.Distance
-
-				if distance1 >= 0 && suggestionsCombi[0].Distance+1 < distance1 ||
-					(suggestionsCombi[0].Distance+1 == distance1 &&
-						float64(suggestionsCombi[0].Count) > (float64(best1.Count)/s.N)*float64(best2.Count)) {
-					suggestionsCombi[0].Distance += 1
-					suggestionParts[len(suggestionParts)-1] = suggestionsCombi[0]
-					s.ReplacedWords[terms1[i-1]] = suggestionsCombi[0]
-					isLastCombi = true
+				best1 := cp.suggestionParts[len(cp.suggestionParts)-1]
+				best2 := s.getBestSuggestion2(cp, maxEditDistance)
+				skip := s.validateCombinationDistance(best1, best2, suggestionsCombi[0], &cp)
+				if skip {
 					continue
 				}
 			}
 		}
-		isLastCombi = false
+		cp.isLastCombi = false
 
 		// Handle terms with no perfect suggestion
-		if len(suggestions) > 0 && (suggestions[0].Distance == 0 || len(terms1[i]) == 1) {
-			suggestionParts = append(suggestionParts, suggestions[0])
+		if len(cp.suggestions) > 0 && (cp.suggestions[0].Distance == 0 || len(cp.terms1) == 1) {
+			cp.suggestionParts = append(cp.suggestionParts, cp.suggestions[0])
 		} else {
 			var suggestionSplitBest *SuggestItem
-			if len(suggestions) > 0 {
-				suggestionSplitBest = &suggestions[0]
+			if len(cp.suggestions) > 0 {
+				suggestionSplitBest = &cp.suggestions[0]
 			}
-			if len(terms1[i]) > 1 {
-				runes := []rune(terms1[i])
+			if len(cp.terms1) > 1 {
+				runes := []rune(cp.terms1)
 				for j := 1; j < len(runes); j++ {
-					part1 := string(runes[:j])
-					part2 := string(runes[j:])
-					suggestions1, _ := s.Lookup(part1, verbositypkg.Top, maxEditDistance)
-					suggestions2, _ := s.Lookup(part2, verbositypkg.Top, maxEditDistance)
-					if len(suggestions1) == 0 || len(suggestions2) == 0 {
+					suggestions1, suggestions2, isValid := s.getSuggestions(runes, j, maxEditDistance)
+					if !isValid {
 						continue
 					}
-					tmpTerm := suggestions1[0].Term + " " + suggestions2[0].Term
-					tmpDistance := s.distanceCompare(terms1[i], tmpTerm, maxEditDistance)
+					cp.suggestion1, cp.suggestion2 = *suggestions1, *suggestions2
+					tmpDistance := s.distanceCompare(cp.terms1, cp.tempTerm(), maxEditDistance)
 					if tmpDistance < 0 {
 						tmpDistance = maxEditDistance + 1
 					}
@@ -100,53 +90,100 @@ func (s *SymSpell) LookupCompound(phrase string, maxEditDistance int) *SuggestIt
 					}
 
 					// Check for bigrams
-					var tmpCount int
-					if count, exists := s.Bigrams[tmpTerm]; exists {
-						tmpCount = count
+					tmpCount := s.checkForBigram(&cp)
 
-						// Update count if split corrections match
-						if len(suggestions) > 0 {
-							bestSI := suggestions[0]
-							if suggestions1[0].Term+suggestions2[0].Term == terms1[i] {
-								tmpCount = int(math.Max(float64(tmpCount), float64(bestSI.Count+2)))
-							} else if bestSI.Term == suggestions1[0].Term || bestSI.Term == suggestions2[0].Term {
-								tmpCount = int(math.Max(float64(tmpCount), float64(bestSI.Count+1)))
-							}
-						} else if suggestions1[0].Term+suggestions2[0].Term == terms1[i] {
-							tmpCount = int(math.Max(
-								float64(tmpCount),
-								math.Max(
-									float64(suggestions1[0].Count),
-									float64(suggestions2[0].Count),
-								)+2,
-							))
-						}
-					} else {
-						// Calculate Naive Bayes probability-based count
-						tmpCount = int(math.Min(
-							float64(s.BigramCountMin),
-							float64(suggestions1[0].Count)/s.N*float64(suggestions2[0].Count),
-						))
-					}
-
-					splitSuggestion := SuggestItem{Term: tmpTerm, Distance: tmpDistance, Count: tmpCount}
+					splitSuggestion := SuggestItem{Term: cp.tempTerm(), Distance: tmpDistance, Count: tmpCount}
 					if suggestionSplitBest == nil || splitSuggestion.Count > suggestionSplitBest.Count {
 						suggestionSplitBest = &splitSuggestion
 					}
 				}
 			}
 			if suggestionSplitBest != nil {
-				suggestionParts = append(suggestionParts, *suggestionSplitBest)
-				s.ReplacedWords[terms1[i]] = *suggestionSplitBest
+				cp.updateReplaceWord(terms1[i], *suggestionSplitBest)
 			} else {
 				item := createWithProbability(terms1[i], maxEditDistance+1)
-				suggestionParts = append(suggestionParts, item)
-				s.ReplacedWords[terms1[i]] = item
+				cp.updateReplaceWord(terms1[i], item)
 			}
 		}
 	}
 
-	// Combine final suggestions
+	return s.finalizeAnswer(phrase, cp.suggestionParts)
+}
+
+func (s *SymSpell) getBestSuggestion2(cp compoundProcessor, maxEditDistance int) SuggestItem {
+	var best2 SuggestItem
+	if len(cp.suggestions) > 0 {
+		best2 = cp.suggestions[0]
+	} else {
+		best2 = createWithProbability(cp.terms1, maxEditDistance+1)
+	}
+	return best2
+}
+
+func (s *SymSpell) validateCombinationDistance(best1 SuggestItem, best2 SuggestItem, suggestionsCombine SuggestItem, cp *compoundProcessor) bool {
+	distance1 := best1.Distance + best2.Distance
+
+	if distance1 >= 0 && suggestionsCombine.Distance+1 < distance1 ||
+		(suggestionsCombine.Distance+1 == distance1 &&
+			float64(suggestionsCombine.Count) > (float64(best1.Count)/s.N)*float64(best2.Count)) {
+		suggestionsCombine.Distance += 1
+		cp.suggestionParts[len(cp.suggestionParts)-1] = suggestionsCombine
+		cp.replacedWords[cp.terms2] = suggestionsCombine
+		cp.isLastCombi = true
+		return true
+	}
+	return false
+}
+
+func (s *SymSpell) getSuggestions(runes []rune, split int, maxEditDistance int) (*SuggestItem, *SuggestItem, bool) {
+	part1 := string(runes[:split])
+	part2 := string(runes[split:])
+	suggestions1, _ := s.Lookup(part1, verbositypkg.Top, maxEditDistance)
+	suggestions2, _ := s.Lookup(part2, verbositypkg.Top, maxEditDistance)
+	if len(suggestions1) == 0 || len(suggestions2) == 0 {
+		return nil, nil, false
+	}
+	return &suggestions1[0], &suggestions2[0], true
+}
+
+func (s *SymSpell) checkForBigram(cp *compoundProcessor) int {
+	var tmpCount int
+	if count, exists := s.Bigrams[cp.tempTerm()]; exists {
+		tmpCount = count
+
+		// Update count if split corrections match
+		if len(cp.suggestions) > 0 {
+			bestSI := cp.suggestions[0]
+			if cp.suggestion1.Term+cp.suggestion2.Term == cp.terms1 {
+				tmpCount = int(math.Max(float64(tmpCount), float64(bestSI.Count+2)))
+			} else if bestSI.Term == cp.suggestion1.Term || bestSI.Term == cp.suggestion2.Term {
+				tmpCount = int(math.Max(float64(tmpCount), float64(bestSI.Count+1)))
+			}
+		} else if cp.suggestion1.Term+cp.suggestion2.Term == cp.terms1 {
+			tmpCount = int(math.Max(
+				float64(tmpCount),
+				math.Max(
+					float64(cp.suggestion1.Count),
+					float64(cp.suggestion2.Count),
+				)+2,
+			))
+		}
+	} else {
+		// Calculate Naive Bayes probability-based count
+		tmpCount = int(math.Min(
+			float64(s.BigramCountMin),
+			float64(cp.suggestion1.Count)/s.N*float64(cp.suggestion2.Count),
+		))
+	}
+	return tmpCount
+}
+
+func (c *compoundProcessor) updateReplaceWord(terms1 string, item SuggestItem) {
+	c.suggestionParts = append(c.suggestionParts, item)
+	c.replacedWords[terms1] = item
+}
+
+func (s *SymSpell) finalizeAnswer(phrase string, suggestionParts []SuggestItem) *SuggestItem {
 	joinedTerm := ""
 	joinedCount := s.N
 	for _, item := range suggestionParts {
@@ -154,13 +191,12 @@ func (s *SymSpell) LookupCompound(phrase string, maxEditDistance int) *SuggestIt
 		joinedCount *= float64(item.Count) / s.N
 	}
 	joinedTerm = strings.TrimSpace(joinedTerm)
-	finalSuggestion := SuggestItem{
+
+	return &SuggestItem{
 		Term:     joinedTerm,
 		Distance: s.distanceCompare(phrase, joinedTerm, math.MaxInt32),
 		Count:    int(joinedCount),
 	}
-
-	return &finalSuggestion
 }
 
 func createWithProbability(term string, distance int) SuggestItem {
@@ -254,4 +290,19 @@ func (s *SymSpell) LoadBigramDictionary(
 
 	// Use the stream-based loading function
 	return s.LoadBigramDictionaryStream(file, termIndex, countIndex, separator), nil
+}
+
+type compoundProcessor struct {
+	suggestions     []SuggestItem
+	suggestionParts []SuggestItem
+	replacedWords   map[string]SuggestItem
+	terms1          string
+	terms2          string
+	suggestion1     SuggestItem
+	suggestion2     SuggestItem
+	isLastCombi     bool
+}
+
+func (c *compoundProcessor) tempTerm() string {
+	return fmt.Sprintf("%s %s", c.suggestion1.Term, c.suggestion2.Term)
 }
